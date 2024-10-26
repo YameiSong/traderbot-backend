d86bee44a27acc49c8345d733fad788cb419e984
Yamei Song <ymsongx@163.com>
2024-10-26 21:54:28 +1100

    Add initial Spring Boot project setup
    This commit includes the essential files for a new Spring Boot project. It adds a HomeController, application properties, dependencies management via the pom.xml, Maven wrapper files, initial project structure, and basic configurations.

---
+++ src/main/java/com/cryptoai/traderbot/TraderbotApplication.java
@@ -0,0 +1,13 @@
+package com.cryptoai.traderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptoai/traderbot/TraderbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptoai.traderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
