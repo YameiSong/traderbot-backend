38c24ced749f59a4d1d416f83dfa5b294a03374d
Yamei Song <ymsongx@163.com>
2025-03-28 14:18:37 +1100

    fix build dependency issue

--- src/main/java/com/cryptoai/traderbot/service/ChatbotServiceImpl.java
+++ src/main/java/com/cryptoai/traderbot/service/ChatbotServiceImpl.java
@@ -1,6 +1,5 @@
 package com.cryptoai.traderbot.service;
 
-import com.cryptoai.traderbot.response.ApiResponse;
 import com.cryptoai.traderbot.response.FunctionResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
